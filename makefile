2e81cf3d104424360e90f2969a12dd7f4ff8d4d9
Jakub Kretkowski <kreciak09+github@gmail.com>
2025-06-05 14:49:17 +0200

    add basics

---
+++ makefile
@@ -0,0 +1,6 @@
+env:
+	mkdir obj
+	mkdir bin
+
+all:
+	gcc -o bin/BonkClicker.exe source/main.c -lgdi32 -mwindows
---
+++ source/main.c
@@ -0,0 +1,90 @@
+/*
+	BonkClicker - a goofy clicker for WinAPI
+	Made by Applehat. (github.com/ApplehatDot)
+	
+*/ 
+
+#include <windows.h>
+#include <stdio.h>
+#include "PredFunc.c"
+
+HWND hClicka, hText;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+	switch(uMsg){
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		case WM_CREATE:
+			hText = CreateWindowExW(
+				0,
+				L"STATIC",
+				L"Dis a text",
+				WS_CHILD | WS_VISIBLE,
+				50, 50, 130, 20,
+				hwnd,
+				NULL,
+				GetModuleHandle(NULL),
+				NULL
+			);
+			
+			SetSystemThemeFont(hText);
+			
+			hClicka = CreateWindowExW(
+				0, 
+				L"BUTTON",
+				L"bonk",
+				WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+				70, 100, 130, 130,
+				hwnd,
+				(HMENU)100, 
+				GetModuleHandle(NULL), 
+				NULL
+			);
+			break;
+		
+		default:
+			return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+	}
+	
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	const wchar_t CLASS_NAME[] = L"MainClickWnd";
+	
+	WNDCLASSW wc = {};
+	
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	
+	if(!RegisterClassW(&wc)){
+		MessageBoxW(NULL, L"Window Registration Failed", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+	
+	HWND hwnd = CreateWindowExW(
+		0,
+		CLASS_NAME,
+		L"BonkClicker",
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		300, 430,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	
+	MSG msg = {};
+	while(GetMessage(&msg, NULL, 0, 0) > 0){
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+	
+}
